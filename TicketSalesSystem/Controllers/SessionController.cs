78fad292db2929cc428d426c3787590022c90932
Arseniy.Zhyvitski <Arseniy.Zhyvitski@WSA-234-73>
2021-07-02 15:58:41 +0300

    adding session creation

---
+++ TicketSalesSystem/Controllers/SessionController.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using TicketSalesSystem.Services;
+using TicketSalesSystem.Services.Interfaces;
+
+namespace TicketSalesSystem.Controllers
+{
+    public class SessionController : Controller
+    {
+        private readonly IFilmService _filmService;
+        private readonly ICinemaService _cinemaService;
+        //private readonly ISessionService _sessionService;
+
+        public SessionController(IFilmService filmService, ICinemaService cinemaService)
+        {
+            _filmService = filmService;
+            _cinemaService = cinemaService;
+            //_sessionService = sessionService;
+        }
+
+        [HttpGet]
+        public IActionResult CreateSession(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var filmName = _filmService.GetFilmName(id.Value);
+            var cinemas = _cinemaService.GetCinemaList();
+
+            ViewBag.FilmName = filmName;
+            ViewBag.Cinemas = cinemas;
+
+            return View();
+        }
+    }
+}
---
+++ TicketSalesSystem/Services/Interfaces/ISessionService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+using TicketSalesSystem.ViewModels;
+
+namespace TicketSalesSystem.Services.Interfaces
+{
+    public interface ISessionService
+    {
+        string GetFilmName(int filmId);
+
+    }
+}
